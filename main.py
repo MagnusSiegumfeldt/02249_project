24685fd8068652139b5b8665778054037c6fe3e5
Magnus Siegumfeldt <m.ks@outlook.dk>
2023-10-11 10:13:02 +0200

    preprocess

--- main.py
+++ main.py
@@ -11,48 +11,39 @@ def check(s, strings, assigns):
     print(assigns)
     return True
 
-
 def explore(s, strings, expansions, assigns, contained_letters):
     if len(expansions) == 0:
         return check(s, strings, assigns)
     letter = list(expansions.keys())[0]
     expansion_list = expansions[letter]
-    
 
-    # Skip all expansions if letter not in any string
-    if letter not in contained_letters:
+    for expansion in expansion_list:
+        assigns[letter] = expansion
         expansions_copy = expansions.copy()
         expansions_copy.pop(letter)
         if explore(s, strings, expansions_copy, assigns, contained_letters):
             return True
-    else: 
-        for expansion in expansion_list:
-            assigns[letter] = expansion
-            expansions_copy = expansions.copy()
-            expansions_copy.pop(letter)
-            if explore(s, strings, expansions_copy, assigns, contained_letters):
-                return True
-            assigns.pop(letter)
+        assigns.pop(letter)
 
     return False
 
 # Removes expansions which contains letters not in s
-def preprocess(s, expansions):
+def preprocess(s, letters, expansions):
     new_expansions = {}
 
     keys = list(expansions.keys())
     for key in keys:
-        new_exp = []
-        expansions_list = expansions[key]
-        for e in expansions_list:
-            if e in s:
-                new_exp.append(e)
-        new_expansions[key] = new_exp
+        if key in letters:
+            new_exp = []
+            expansions_list = expansions[key]
+            for e in expansions_list:
+                if e in s:
+                    new_exp.append(e)
+            new_expansions[key] = new_exp
     return new_expansions
 
 # Setup and input
 Gamma = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-Sigma = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 
 n = int(input())
 s = input()
@@ -72,5 +63,5 @@ for _ in range(26): # TODO: change this.
     expansions[letter] = exp.split(",")
 
 assigns = {}
-expansions = preprocess(s, expansions)
+expansions = preprocess(s, contained_letters, expansions)
 print(explore(s, strings, expansions, assigns, contained_letters))
